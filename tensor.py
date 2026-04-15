a00bdf04be5111a75eb6d401c435678b1c59fa27
samvarshah <samvarshah0907@gmail.com>
2026-04-15 17:15:31 +0530

    implemented minimal autograd engine with backward pass

--- tensor.py
+++ tensor.py
@@ -2,13 +2,6 @@ import numpy as np
 
 
 class Tensor:
-    """
-    requiring gradient is a default parameter set to false
-    data -> actual value
-    zeros_like -> array to track grad set to 0
-    _prev -> parent input
-    op -> operation
-    """
     def __init__(self, data, requires_grad=False, _children=(), _op=""):
         self.data = np.array(data, dtype=float)
         self.requires_grad = requires_grad
@@ -18,6 +11,7 @@ class Tensor:
             self.grad = None
         self._prev = set(_children)
         self._op = _op
+        self._backward = lambda: None
 
     def __add__(self, other):
         if isinstance(other, Tensor):  # allows you to do x + 5
@@ -30,6 +24,14 @@ class Tensor:
             _children=(self, other),
             _op="+",
         )
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += out.grad
+            if other.requires_grad:
+                other.grad += out.grad
+
+        out._backward = _backward
         return out
 
     def __mul__(self, other):
@@ -43,17 +45,57 @@ class Tensor:
             _children=(self, other),
             _op="*",
         )
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += other.data * out.grad
+            if other.requires_grad:
+                other.grad += self.data * out.grad
+
+        out._backward = _backward
         return out
 
     def matmul(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
+        if isinstance(other, Tensor):
+            other = other
+        else:
+            other = Tensor(other)
         out = Tensor(
             self.data @ other.data,
             requires_grad=self.requires_grad or other.requires_grad,
             _children=(self, other),
             _op="matmul",
         )
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += out.grad @ other.data.T
+            if other.requires_grad:
+                other.grad += self.data.T @ out.grad
+
+        out._backward = _backward
         return out
+
+    def backward(self):
+        if not self.requires_grad:
+            return
+
+        topo = []
+        visited = set()
+
+        def build_topo(t):
+            if t not in visited:
+                visited.add(t)
+                for child in t._prev:
+                    build_topo(child)
+                topo.append(t)
+
+        build_topo(self)
+
+        self.grad = np.ones_like(self.data)
+
+        for t in reversed(topo):
+            t._backward()
 
     def __repr__(self):
         return f"Tensor(data={self.data}, op={self._op}, requires_grad={self.requires_grad})"
@@ -70,5 +112,7 @@ x = Tensor(2.0, requires_grad=True)
 y = Tensor(3.0, requires_grad=True)
 z = x * y + x
 
-print(z)
-print(z.history())
+z.backward()
+print("z.data =", z.data)
+print("x.grad =", x.grad)
+print("y.grad =", y.grad)
